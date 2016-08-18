1f4c8bab4ec0c9fb52002fe37755f58ccb8199b3
jesse houghton <OiseauNoir>
2016-08-18 15:16:15 -0500

    Add students route

--- routes/students.js
+++ routes/students.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Student = require('../models/Student')
+
 
 router.get('/', function(req, res, next) {
-  
+  Student.find({}, function(err, students) {
+    if (err) {
+      res.status(500).send()
+    } else {
+      res.json(students)
+    }
+  })
 });
 
 module.exports = router;

9d3d431c0b4c5d5f93f527b2c08aebdbb0758d52
jesse houghton <OiseauNoir>
2016-08-18 15:27:16 -0500

    Add whitelist middleware

--- routes/students.js
+++ routes/students.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var Student = require('../models/Student')
+const _ = require('lodash');
+
+router.use(function (req, res, next) {
+  req.body = _.pick(req.body, ['firstName', 'lastName', 'salary', 'bonusPoints'])
+  next()
+})
 
 
 router.get('/', function(req, res, next) {
